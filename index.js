d10c1cced3d19f4f29a77e4967ccbc0c1e112728
Josh Campbell <joshcb@amazon.com>
2016-06-17 09:40:30 -0500

    error handling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*jslint node: true */
-
+/*jshint multistr: true */
 'use strict';
 
 var AlexaSkill = require('./AlexaSkill'),
@@ -22,7 +22,6 @@ Alexa.prototype = Object.create(AlexaSkill.prototype);
 Alexa.prototype.constructor = Alexa;
 
 Alexa.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    /*jshint multistr: true */
     var speechText = "Hello, Date Night can you help find an activity or \
       restaurant. You can ask for a suggestion like, where should we go for \
       brunch in Chicago? or for an outdoor activity suggestion in Chicago? \
@@ -36,72 +35,84 @@ Alexa.prototype.eventHandlers.onLaunch = function (launchRequest, session, respo
 Alexa.prototype.intentHandlers = {
     "DateNightIntent": function (intent, session, response) {
         console.log('DateNightIntent starting...');
-        var city = intent.slots.City.value.toLowerCase(),
-            activity = intent.slots.Activity.value.toLowerCase();
+        var city = intent.slots.City.value,
+            activity = intent.slots.Activity.value,
+            speech,
+            selection,
+            speechOutput,
+            repromptOutput = {
+                speech: "You can try asking me where you should go for \
+                  dinner in Chicago as an example.",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            },
+            cardTitle = "Date Night Suggestion";
+
         console.log('======Input Debugging:');
         console.log(city);
         console.log(activity);
 
-        var cardTitle = "Date Night Suggestion",
-            selection = activities[city][activity][
-              Math.floor(Math.random()*activities[city][activity].length)],
-            speechOutput,
-            repromptOutput;
+        if (city && activity) {
+          try {
+              selection = activities[city.toLowerCase()][activity.toLowerCase()][
+                  Math.floor(Math.random()*activities[
+                      city.toLowerCase()][activity.toLowerCase()].length)];
+          } catch (err) {} // TODO better error handling when looking up activities
+        }
+
         console.log('=====Selection Debugging:');
         console.log(selection);
         if (selection) {
+            console.log('Found a selection...');
             speechOutput = {
                 speech: selection,
                 type: AlexaSkill.speechOutputType.PLAIN_TEXT
             };
             response.tellWithCard(speechOutput, cardTitle, selection);
-        } else {
-            var speech;
-            if (selection) {
-                /*jshint multistr: true */
-                speech = "I'm sorry, I can't find a Date Night suggestion for \
-                  that activity or city. What can I help you find?";
-            } else {
-                /*jshint multistr: true */
-                speech = "I'm sorry, please try asking me for a Date Night \
-                  suggestion again.";
-            }
+        } else if (!selection && !city) {
+            console.log('No city specified!');
             speechOutput = {
-                speech: speech,
+                speech: "Sorry, you must specify a city.  Try Chicago, for example.",
                 type: AlexaSkill.speechOutputType.PLAIN_TEXT
             };
-            repromptOutput = {
-                speech: "What else can I help you find?",
+            response.ask(speechOutput, repromptOutput);
+        } else if (!selection && !activity) {
+            console.log('No activity specified!');
+            speechOutput = {
+                speech: "Sorry, you must specify an activity.  Try saying dinner \
+                    or indoor activity, for example.",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.ask(speechOutput, repromptOutput);
+        } else {
+            console.log('Something weird happened.');
+            speechOutput = {
+                speech: "I'm sorry, please try asking me for a Date Night \
+                  suggestion again.",
                 type: AlexaSkill.speechOutputType.PLAIN_TEXT
             };
             response.ask(speechOutput, repromptOutput);
         }
-        console.log('DateNightIntent finished.');
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
         console.log('StopIntent starting...');
         var speechOutput = "Goodbye";
         response.tell(speechOutput);
-        console.log('StopIntent finished.');
     },
 
     "AMAZON.CancelIntent": function (intent, session, response) {
         console.log('CancelIntent starting...');
         var speechOutput = "Goodbye";
         response.tell(speechOutput);
-        console.log('CancelIntent finished.');
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
         console.log('HelpIntent starting...');
-        /*jshint multistr: true */
         var speechText = "You can ask me for suggestions on activities in \
-        your city. For example try asking me, where should we go for a dive bar \
+        your city. For example, try asking me, where should we go for a dive bar \
         in Chicago? ..., or, you can say exit... Now, what can I help you find?";
-        /*jshint multistr: true */
         var repromptText = "You can say things like, what activity in Chicago \
-          should we do, or you can say exit... Now, what can I help you with?";
+          should we do? ..., or, you can say exit... Now, what can I help you with?";
         var speechOutput = {
             speech: speechText,
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
