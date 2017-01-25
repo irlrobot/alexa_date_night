b5cd11f91257ebe1b525dabfd7d33e58e0abdc6c
Josh Campbell <josh@userdel.com>
2017-01-24 22:45:21 -0800

    fix bug in speech looking for length of cuisine

--- src/handlers/en-us.handler.js
+++ src/handlers/en-us.handler.js
@@ -6,8 +6,8 @@ module.exports = {
   'DateNightIntent': function () {
     console.log("DateNightIntent fired...");
     var cuisine = cuisines[Math.floor(Math.random()*cuisines.length)];
-        speech = phrases[Math.floor(Math.random()*cuisines.length)];
-    this.emit(':tellWithCard', speech + cuisine, SKILL_NAME, cuisine);
+        speech = phrases[Math.floor(Math.random()*phrases.length)] + cuisine;
+    this.emit(':tellWithCard', speech, SKILL_NAME, cuisine);
   },
   'AMAZON.CancelIntent': function () {
     console.log("CancelIntent fired...");
