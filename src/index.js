b8a95fdcffa5be5743981e8b8184073224874f20
Josh Campbell <joshcb@amazon.com>
2017-01-26 22:03:20 -0800

    moves ddb to top

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ var ALEXA = require('alexa-sdk');
     AWS = require("aws-sdk");
     APP_ID = "amzn1.echo-sdk-ams.app.24d90ad8-0518-4495-b96d-2a77833245b1";
     SKILL_NAME = "Date Night";
+    DDB_TABLE = 'date_night_prod';
+    DDB_CLIENT = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function(event, context, callback) {
   var userId = event.session.user.userId;
-      // DynamoDB
-      ddbClient = new AWS.DynamoDB.DocumentClient();
-      ddbTable = 'date_night_prod';
-      // Handlers
       alexa = ALEXA.handler(event, context);
       enUsHandlers = require('./handlers/en-us.handler')(
         userId,
-        ddbClient,
-        ddbTable
+        DDB_CLIENT,
+        DDB_TABLE
       );
-      // Date Night
       cuisines = require('./modules/cuisines');
       phrases = require('./modules/phrases');
 
