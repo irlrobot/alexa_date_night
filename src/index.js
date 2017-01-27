17380664efe0a80821c191dc69c3651af3b0bd0c
Josh Campbell <joshcb@amazon.com>
2017-01-26 16:02:51 -0800

    adds aws sdk, ddb support, and more logging

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-var Alexa = require('alexa-sdk');
+var ALEXA = require('alexa-sdk');
+    AWS = require("aws-sdk");
     APP_ID = "amzn1.echo-sdk-ams.app.24d90ad8-0518-4495-b96d-2a77833245b1";
     SKILL_NAME = "Date Night";
-    enUsHandlers = require('./handlers/en-us.handler');
-    cuisines = require('./modules/cuisines');
-    phrases = require('./modules/phrases');
 
 exports.handler = function(event, context, callback) {
+  var userId = event.session.user.userId;
+      // DynamoDB
+      ddbClient = new AWS.DynamoDB.DocumentClient();
+      ddbTable = 'date_night_prod';
+      // Handlers
+      alexa = ALEXA.handler(event, context);
+      enUsHandlers = require('./handlers/en-us.handler')(
+        userId,
+        ddbClient,
+        ddbTable
+      );
+      // Date Night
+      cuisines = require('./modules/cuisines');
+      phrases = require('./modules/phrases');
+
+  // Logging for debugging
   console.log("Starting Date Night...");
+  console.log("UserID is:  " + userId);
   console.log("Intent is:  " + event.request.intent);
-  var alexa = Alexa.handler(event, context);
-  var locale = event.request.locale;
-  console.log("Locale is:  " + locale);
+  console.log("Locale is:  " + event.request.locale);
+
+  // Invoke skill
   alexa.appId = APP_ID;
   console.log("Registering en-us.handler...");
   alexa.registerHandlers(enUsHandlers);
