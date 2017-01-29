ebb3a279db9586d836486c6f50a36fc170209dcb
Josh Campbell <joshcb@amazon.com>
2017-01-28 19:42:24 -0800

    adds new phrases

--- src/modules/phrases.js
+++ src/modules/phrases.js
@@ -1,4 +1,6 @@
 module.exports = [
   "Looks like a great night for some ",
-  "Let's shake it up a bit, how about some "
+  "Let's shake it up a bit, how about some ",
+  "Why don't you guys grab some ",
+  "I have an idea, how about some "
 ];
